bbfb78c008ac56f4cdff483994fe329fbfa24c32
xChen16 <ares_0427@163.com>
2021-05-28 16:33:33 +0800

    service

--- service/service.go
+++ service/service.go
@@ -1,1 +1,32 @@
 package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Start(ctx context.Context, serviceName, host, port string, registerHandlersFunc func()) (context.Context, error) {
+	registerHandlersFunc()
+	ctx = startService(ctx, serviceName, host, port)
+	return ctx, nil
+}
+
+func startService(ctx context.Context, serviceName, host, port string) context.Context {
+	ctx, cancel := context.WithCancel(ctx)
+	var srv http.Server
+	srv.Addr = ":" + port
+	go func() {
+		log.Println(srv.ListenAndServe())
+		cancel()
+	}()
+	go func() {
+		fmt.Printf("%v started \n", serviceName)
+		var s string
+		fmt.Scanln(&s)
+		srv.Shutdown(ctx)
+		cancel()
+	}()
+	return ctx
+}
