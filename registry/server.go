8afa040c207bf6dd740c98129f1769570a142047
xChen16 <ares_0427@163.com>
2021-06-13 16:02:37 +0800

    add remove

--- registry/server.go
+++ registry/server.go
@@ -2,6 +2,8 @@ package registry
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"sync"
@@ -22,6 +24,19 @@ func (r *registry) add(reg Registration) error {
 	r.registrations = append(r.registrations, reg)
 	r.mutex.Unlock()
 	return nil
+}
+
+func (r *registry) remove(url string) error {
+	for i := range reg.registrations {
+		if reg.registrations[i].ServiceURL == url {
+			r.mutex.Lock()
+			reg.registrations = append(reg.registrations[:i], reg.registrations[:i+1]...)
+			r.mutex.Unlock()
+			return nil
+
+		}
+	}
+	return fmt.Errorf("service at url %s not found", url)
 }
 
 var reg = registry{
@@ -49,6 +64,21 @@ func (s RegistryService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	case http.MethodDelete:
+		payload, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		url := string(payload)
+		log.Printf("remove service at url: %s ", url)
+		err = reg.remove(url)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 	default:
